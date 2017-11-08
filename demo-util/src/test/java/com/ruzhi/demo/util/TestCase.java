8ef786c307a1ae1e26308b1c8ca36dcc945eed90
chunchong.wchl <chunlong.wchl@alibaba-inc.com>
2017-11-08 18:32:33 +0800

    ceshi_ruzhi

---
+++ demo-util/src/test/java/com/ruzhi/demo/util/TestCase.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package com.ruzhi.demo.util;
+
+/**
+ *
+ * @author chunlong.wchl
+ * @version $Id: TestCase.java, v 0.1 2017年11月08日 下午6:27 chunlong.wchl Exp $
+ */
+public class TestCase {
+}
